ee198315cd7d915aec5d7a5fd845a0403d3f6beb
Ramakrishnan CS <ram95krishnan@gmail.com>
2025-09-01 15:17:39 +0100

    feat(api): allow CORS for frontend

--- api.py
+++ api.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from sqlalchemy import func
 from database import SessionLocal, Trade, Holding, SentimentStat
 import datetime
 
 app = FastAPI()
+
+# Allow cross-origin requests in local development
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def get_db():
